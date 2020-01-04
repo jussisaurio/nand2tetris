a01b67db977ac7bed95fd9e8c4daf745e7c3c472
Jussi Saurio <jussi.saurio@gmail.com>
2020-01-04 12:14:14 +0200

    Finish week 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,81 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Put your code here.        
+
+@draw // init draw color as black
+M=0
+@color // init screen color as black
+M=0
+@8192 // number of bytes to iterate through
+D=A
+@max
+M=D
+
+(LOOP) // main loop where check key pressed
+@offset
+M=0
+@KBD // load index of keyboard map into A
+D=M // load RAM[KEYBOARD] into D. 0 is black, anything else is white
+@SETBLACK
+D; JEQ
+
+(SETWHITE)
+@draw
+M=-1
+@CHECK
+0; JMP
+
+(SETBLACK)
+@draw
+M=0
+
+(CHECK)
+@draw
+D=M
+@color
+D=D-M
+@LOOP // if draw color is same as screen color, no-op
+D; JEQ
+
+@draw
+D=M
+@color
+M=D
+
+@draw
+D=M
+@PAINTBLACKLOOP
+D; JEQ
+
+(PAINTWHITELOOP)
+@offset
+D=M
+@SCREEN
+A=A+D
+M=-1
+@offset
+M=M+1
+D=M
+@max
+D=M-D
+@LOOP
+D; JEQ
+@PAINTWHITELOOP
+0; JMP
+
+(PAINTBLACKLOOP)
+@offset
+D=M
+@SCREEN
+A=A+D
+M=0
+@offset
+M=M+1
+D=M
+@max
+D=M-D
+@LOOP
+D; JEQ
+@PAINTBLACKLOOP
+0; JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@sum
+M=0
+
+@1
+D=M
+
+@i
+M=D
+
+(LOOP)
+@i
+D=M
+@END
+D; JEQ
+
+@0
+D=M
+@sum
+M=D+M
+@i
+M=M-1
+@LOOP
+0; JMP
+(END)
+@sum
+D=M
+@2
+M=D
+
